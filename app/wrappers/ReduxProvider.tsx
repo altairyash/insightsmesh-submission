5d5836db6e07ac689fc87239fb8340a14f0f45a7
Yash <yash140498@gmail.com>
2025-07-06 02:03:57 +0530

    change prpject struture

---
+++ app/wrappers/ReduxProvider.tsx
@@ -0,0 +1,6 @@
+"use client";
+import ReduxProvider from "../../store/Provider";
+
+export default function AppReduxProvider({ children }: { children: React.ReactNode }) {
+  return <ReduxProvider>{children}</ReduxProvider>;
+}
